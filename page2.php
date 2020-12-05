48776be0c06f83cd4887d2ba761ece5a65d7b39d
Jordan Parks <jparks@odin.cs.csubak.edu>
2020-12-04 20:06:30 -0800

    Chat room page

---
+++ page2.php
@@ -0,0 +1,43 @@
+<?php
+define("PAGE_TITLE", "Chat Room");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<link href="style.css" rel="stylesheet" type="text/css">
+<!--constant PAGE_TITLE-->
+<title> <?php echo PAGE_TITLE; ?>
+</title>
+</head>
+<body>
+~
+
+<div class='wrapper'>
+
+<div class='header'>
+<h1>Welcome to the Chat Room</h1>
+</div>
+<div class="leftMenu">
+<nav>
+<ul>
+<li><a href="./page2.php">Home</a></li>
+<li><a href="./page2.php">Profile</a></li>
+<li><a href="./page2.php">Chat Room</a></li>
+<li><a href="./page2.php">Quiz</a></li>
+</ul>
+</nav>
+</div>
+
+<div class='content'>
+
+</div>
+
+<footer>
+<div class='footer'>
+</div>
+</footer>
+</div>
+</body>
+</html>
+
