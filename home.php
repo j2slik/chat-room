7a33e2e09f66961acbde7bc5d7f0dc3aedf6fcf5
Jordan Parks <jparks@odin.cs.csubak.edu>
2020-12-15 12:53:10 -0800

    added chatbox

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+define("PAGE_TITLE", "Chat Room");
+?>
+<!doctype html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+<link href="style.css" rel="stylesheet" type="text/css">
+<!--constant PAGE_TITLE-->
+<title> <?php echo PAGE_TITLE; ?>
+</title>
+</head>
+<body>
+~
+
+<div class='wrapper'>
+
+<div class='header'>
+<h1>Welcome to the Chat Room</h1>
+</div>
+<div class='content'>
+<div id="chatbox"> 
+</div>
+</div>
+</div>
+</body>
+</html>
+
